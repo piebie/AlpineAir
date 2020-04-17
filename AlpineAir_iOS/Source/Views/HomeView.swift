c0813e16d5f784de3f02188aaa90f37a4fa42ffa
Pete Biencourt <petebiencourt@gmail.com>
2020-04-16 20:11:27 -0400

    Week 5 Start

---
+++ AlpineAir_iOS/Source/Views/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  AlpineAir_iOS
+//
+//  Created by Pete Biencourt on 4/16/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+// TODO: WEEK 5
+//  1. Implement a TabView with four items:
+//      * Home, with system image house.fill
+//      * Trips, with system image airplane
+//      * Elite, with system image moon.stars.fill
+//      * Profile, with system image person.fill
+//  2. Over the TabView, place a button with a cog image aligned to top trailing (add space as you see fit)
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
